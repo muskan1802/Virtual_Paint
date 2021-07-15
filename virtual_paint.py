aa3195e0da0707a7b0882380eb5c6c591679a385
muskan <muskan1802agrawal@gmail.com>
2021-07-15 21:55:10 +0530

    Initial commit

---
+++ virtual_paint.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+cap.set(10,100)
+
+mycolors = [[104,104,0,179,255,255],[0,0,0,179,33,89]]
+
+mycolorvalues = [[0,0,255],[0,0,0]]
+
+mypts = []     #[x,y,colorid]
+
+def findcolor(img,mycolors,mycolorvalues):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    count=0;
+    newPoints = []
+    for color in mycolors:
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV, lower, upper)
+        x,y=getContours(mask)
+        cv2.circle(imgResult,(x,y),15,mycolorvalues[count],cv2.FILLED)
+        if x!=0 and y!=0:
+            newPoints.append([x,y,count])
+        count+=1
+        #cv2.imshow(str(color[0]),mask)
+    return newPoints
+
+
+def getContours(img):
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    x,y,w,h=0,0,0,0
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area>500:
+            #cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            x , y,w,h = cv2.boundingRect(approx)
+    return x+w//2,y
+
+def drawoncanvas(mypts,mycolorvalues):
+    for points in mypts:
+        cv2.circle(imgResult, (points[0], points[1]), 10, mycolorvalues[points[2]], cv2.FILLED)
+
+while True:
+    success,img = cap.read()
+    #img=cv2.imread(r'C:\Users\muska\Downloads\lena.png')
+    imgResult = img.copy()
+    newPoints = findcolor(img, mycolors,mycolorvalues)
+    if len(newPoints)!=0:
+        for newp in newPoints:
+            mypts.append(newp)
+
+    if len(mypts)!=0:
+        drawoncanvas(mypts,mycolorvalues)
+
+    cv2.imshow("Video",imgResult)
+    if cv2.waitKey(1) & 0xFF == ord('m'):
+        break
